fa8f7bf90551eec9743941c1d73d57b9e3dab26f
davidsudar <davidsudar25@gmail.com>
2022-07-11 20:53:54 +1000

    Initial Commit

---
+++ Mediarr/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Mediarr
+//
+//  Created by David Sudar on 11/7/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mediarr/MediarrApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediarrApp.swift
+//  Mediarr
+//
+//  Created by David Sudar on 11/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct MediarrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
